0701577d7c804d9b059079b97478173d1d9c4ccc
aditya <adityagarg.work@gmail.com>
2012-02-24 20:36:56 +0530

    Memory Del

--- java_file/RajamantriChorSIpahi.java
+++ java_file/RajamantriChorSIpahi.java
@@ -39,27 +39,7 @@ public class rajamantri extends Activity {
 		return super.OnKeyDown(keyCode, event);
 	}
 
-	@Override
-	public void OnLowMemory() {
-		AlertDialog dialog = new AlertDialog.Builder(this).create();
-		dialog.setTitle("Low memory!");
-		dialog.setMessage("Too many apps open, kindly close some other apps and try again!");
-		dialog.setButton("Exit", new DialogInterface.OnClickListener() {
-			
-			public void OnClick(DialogInterface dialog, int which) {
-
-				
-				finish();
-				
-			}
-		});
-		
-		dialog.show();
-
-		
-		super.OnLowMemory();
-	}
-
+	
 	@Override
 	protected void OnPause() {
 		// TODO Auto-generated method stub
